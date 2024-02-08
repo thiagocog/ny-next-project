fc8746e822536ee329fbc29e42c7ac07e490e931
Thiago C. de O. Gonçalves <thiago.cgoncalves@al.infnet.edu.br>
2024-02-08 12:44:56 +0000

    starting points page

--- app/Home.jsx
+++ app/Home.jsx
@@ -1,8 +1,48 @@
-export default function Points() {
+import Link from "next/link";
+
+export default function Home() {
   return (
     <>
-      <h1>Points of interest</h1>
-      <p>Points of interest</p>
+      <section className="section-one">
+        <div className="container">
+          <div className="row">
+            <div className="col py-5">
+              <p className="section-one-title">New York is top!</p>
+              <p className="section-one-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo sequi facilis perferendis ea quibusdam iste eum quasi odit corporis. Suscipit in laudantium corrupti? Hic, culpa voluptate velit nulla enim dolores.
+                Dolor perspiciatis deserunt cupiditate tempora deleniti, odit repudiandae accusamus optio in quidem officiis soluta ullam placeat maiores. Iure libero cupiditate at molestias eum recusandae laboriosam inventore, molestiae culpa, enim eius.
+                Saepe eum corporis, voluptatum a quae cum laborum ipsum debitis tenetur inventore enim maiores delectus quidem non earum molestias quo quasi quas odit quos. Esse hic dolorum maxime sequi optio!
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="section-two">
+        <div className="container">
+          <div className="row col py-5 text-center">
+            <h4 className="mb-5">Find the main points of interent of the city</h4>
+            <div className="text-center">
+              <Link href="/points" className="btn btn-outline-light py-3 px-5"><h4 className="mb-0">Points of interest</h4></Link>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="section-three">
+        <div className="container">
+          <div className="row col py-5 text-center">
+            <h4 className="mb-3">Consult the interactive map</h4>
+            <p className="py-3">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt cupiditate ex distinctio consequatur sapiente ipsa tempora voluptatem, ipsum magnam nam provident delectus atque facere magni nihil? Hic dolore at voluptas.
+              Accusantium cum in nam nostrum at illo iure iusto modi accusamus voluptatibus reprehenderit voluptatem quis esse magni possimus tempora ex repudiandae, velit expedita sapiente maiores non. Necessitatibus, blanditiis quos? Sequi!
+            </p>
+            <div className="text-center">
+              <Link href="/map" className="btn btn-outline-dark py-3 px-5"><h4 className="mb-0">Consult the map</h4></Link>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- app/points/page.jsx
+++ app/points/page.jsx
@@ -1,7 +1,37 @@
-export default function Home() {
+export default function Points() {
+
+   // collection of points
+   const points = [
+    {
+        title: "Central Park",
+        description: "Central Park is a public urban park located in the metropolitan district of Manhattan, in New York City, United States. The park has an area of ​​341 hectares, making it one of the largest urban parks in the world, and is managed by the Central Park Conservancy, a non-profit organization, with the consent of the New York City Council.",
+        image: "point_1.png"
+    },
+    {
+        title: "Empire State Building",
+        description: "The Empire State Building is a 102-story Art Deco skyscraper located at the intersection of 5th Avenue and West 34th Street in New York City. Its name derives from the nickname of the state of New York.",
+        image: "point_2.png"
+    },
+    {
+        title: "Estátua da Liberdade",
+        description: "The Statue of Liberty is a colossal neoclassical sculpture on Liberty Island, at the mouth of the Hudson River, in New York, United States. The statue, designed by French sculptor Frédéric Auguste Bartholdi, is a gift from France to the United States in recognition of Franco-American friendship during the American Revolution.",
+        image: "point_3.png"
+    },
+    {
+        title: "Times Square",
+        description: "Times Square is a large commercial square, between 42nd and 47th streets, in New York City, United States. Times Square is an important city entertainment center and tourist attraction. The square is famous for its bright signs and the large crowds that frequent it.",
+        image: "point_4.png"
+    },
+    {
+        title: "Brooklyn Bridge",
+        description: "The Brooklyn Bridge is a cable suspension bridge on the East River that connects the borough of Manhattan to the borough of Brooklyn in New York City. The bridge is one of New York City's most famous landmarks and an icon of American engineering.",
+        image: "point_5.png"
+    }
+]
+
   return (
     <>
-      <h1>Home</h1>
+      
     </>
   );
 }
